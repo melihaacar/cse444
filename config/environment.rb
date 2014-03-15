24fe2c32e56ea85da3541b691564bd6a5d62a07a
Kaan Çelen <bkaancelen@gmail.com>
2014-03-15 11:15:45 +0200

    ikinci yükleme, Üyelik sistemi oluşturuldu.Bireyler siteye üye olup profillerini görüntüleyebiliyor,düzenleyebiliyorlar. Admin sistemi eklendi.Admin kullanıcı tüm üyeleri görebiliyor. Üye izni ile girilen sayfalar oluşturuldu.Bu sayfalarda kullanıcı 'Üye girişi' sayfasına yönlendiriliyor,üye girişi yapılırsa girmek istediği sayfaya geri yönlendiriliyor.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 SampleApp::Application.initialize!
+
+#will_paginate
+require "will_paginate"
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ SampleApp::Application.routes.draw do
   # ruby tutorial listing 7.3
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  root  'sessions#new'
+  root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
